c5354f05bc381ba13b9bec3add5ea1ae4135f70a
Siva <skailas@andrew.cmu.edu>
2022-10-31 20:28:35 -0400

    Add test script for SSD

--- ssd_test.py
+++ ssd_test.py
@@ -13,7 +13,6 @@ pp = PrettyPrinter()
 
 # Data parameters
 data_folder = './'  # folder with data files
-keep_difficult = True  # use objects considered difficult to detect?
 
 # Model parameters
 # Not too many here since the SSD300 has a very specific structure
@@ -21,7 +20,7 @@ n_classes = len(label_map)  # number of different types of objects
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Learning parameters
-checkpoint = None  # path to model checkpoint, None if none
+checkpoint = 'checkpoint_ssd300.pth.tar'  # path to model checkpoint, None if none
 batch_size = 8  # batch size
 iterations = 120000  # number of iterations to train
 workers = 4  # number of workers for loading data in the DataLoader
@@ -70,104 +69,29 @@ def main():
     criterion = MultiBoxLoss(priors_cxcy=model.priors_cxcy).to(device)
 
     # Custom dataloaders
-    train_dataset = HITUAVDatasetTrain(data_folder)
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True,
-                                               collate_fn=train_dataset.collate_fn, num_workers=workers,
-                                               pin_memory=True)  # note that we're passing the collate function here
-    
-    val_dataset = HITUAVDatasetVal(data_folder)
-    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=True,
-                                               collate_fn=val_dataset.collate_fn, num_workers=workers,
-                                               pin_memory=True)  # note that we're passing the collate function here
-    
-    # test_dataset = HITUAVDatasetTest(data_folder)
-    # test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True,
-    #                                            collate_fn=test_dataset.collate_fn, num_workers=workers,
+    # train_dataset = HITUAVDatasetTrain(data_folder)
+    # train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True,
+    #                                            collate_fn=train_dataset.collate_fn, num_workers=workers,
     #                                            pin_memory=True)  # note that we're passing the collate function here
+    
+    # val_dataset = HITUAVDatasetVal(data_folder)
+    # val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=True,
+    #                                            collate_fn=val_dataset.collate_fn, num_workers=workers,
+    #                                            pin_memory=True)  # note that we're passing the collate function here
+    
+    test_dataset = HITUAVDatasetTest(data_folder)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True,
+                                               collate_fn=test_dataset.collate_fn, num_workers=workers,
+                                               pin_memory=True)  # note that we're passing the collate function here
 
     # Calculate total number of epochs to train and the epochs to decay learning rate at (i.e. convert iterations to epochs)
     # To convert iterations to epochs, divide iterations by the number of iterations per epoch
     # The paper trains for 120,000 iterations with a batch size of 32, decays after 80,000 and 100,000 iterations
-    epochs = iterations // (len(train_dataset) // 32)
-    decay_lr_at = [it // (len(train_dataset) // 32) for it in decay_lr_at]
+    # epochs = iterations // (len(train_dataset) // 32)
+    # decay_lr_at = [it // (len(train_dataset) // 32) for it in decay_lr_at]
 
-    # Epochs
-    for epoch in range(start_epoch, epochs):
+    evaluate(test_loader=test_loader, model=model)
 
-        # Decay learning rate at particular epochs
-        if epoch in decay_lr_at:
-            adjust_learning_rate(optimizer, decay_lr_to)
-
-        # One epoch's training
-        train(train_loader=train_loader,
-              model=model,
-              criterion=criterion,
-              optimizer=optimizer,
-              epoch=epoch)
-
-        # Save checkpoint
-        save_checkpoint(epoch, model, optimizer)
-
-        evaluate(test_loader=val_loader, model=model)
-
-
-def train(train_loader, model, criterion, optimizer, epoch):
-    """
-    One epoch's training.
-    :param train_loader: DataLoader for training data
-    :param model: model
-    :param criterion: MultiBox loss
-    :param optimizer: optimizer
-    :param epoch: epoch number
-    """
-    model.train()  # training mode enables dropout
-
-    batch_time = AverageMeter()  # forward prop. + back prop. time
-    data_time = AverageMeter()  # data loading time
-    losses = AverageMeter()  # loss
-
-    start = time.time()
-
-    # Batches
-    for i, (images, boxes, labels) in enumerate(train_loader):
-        data_time.update(time.time() - start)
-
-        # Move to default device
-        images = images.to(device)  # (batch_size (N), 3, 300, 300)
-        boxes = [b.to(device) for b in boxes]
-        labels = [l.to(device) for l in labels]
-
-        # Forward prop.
-        predicted_locs, predicted_scores = model(images)  # (N, 8732, 4), (N, 8732, n_classes)
-
-        # Loss
-        loss = criterion(predicted_locs, predicted_scores, boxes, labels)  # scalar
-
-        # Backward prop.
-        optimizer.zero_grad()
-        loss.backward()
-
-        # Clip gradients, if necessary
-        if grad_clip is not None:
-            clip_gradient(optimizer, grad_clip)
-
-        # Update model
-        optimizer.step()
-
-        losses.update(loss.item(), images.size(0))
-        batch_time.update(time.time() - start)
-
-        start = time.time()
-
-        # Print status
-        if i % print_freq == 0:
-            print('Epoch: [{0}][{1}/{2}]\t'
-                  'Batch Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                  'Data Time {data_time.val:.3f} ({data_time.avg:.3f})\t'
-                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'.format(epoch, i, len(train_loader),
-                                                                  batch_time=batch_time,
-                                                                  data_time=data_time, loss=losses))
-    del predicted_locs, predicted_scores, images, boxes, labels  # free some memory since their histories may be stored
 
 def evaluate(test_loader, model):
     """
